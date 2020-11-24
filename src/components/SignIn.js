6f2122c70802509e1a65d2d5802fa571711812db
Andrei Avram <avramandreitiberiu@gmail.com>
2020-11-24 11:21:12 +0000

    -redeploy

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,25 +8,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import {fetchToken} from "../repositories/AuthRepository";
 
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -63,9 +49,6 @@ export default function SignIn() {
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
                 <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
@@ -128,7 +111,6 @@ export default function SignIn() {
                 </form>
             </div>
             <Box mt={8}>
-                <Copyright />
             </Box>
         </Container>
     );
