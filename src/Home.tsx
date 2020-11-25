44772bc8170e10e81361d0e5f4952dcc25aab56c
Andrei Avram <avramandreitiberiu@gmail.com>
2020-11-25 00:14:33 +0000

    prevent memory leaks

--- src/Home.tsx
+++ src/Home.tsx
@@ -16,48 +16,48 @@ export default  function Home () {
     const [list2, setList2] = useState(new Array<JSX.Element>())
     const [list3, setList3] = useState(new Array<JSX.Element>())
 
+  //
+  // useEffect(()=>{
+  //     fetchRecentProducts().then(result => {
+  //         console.log(result)
+  //         if(result.error === "") {
+  //             let products = result.data
+  //             let mappedProducts = products.map(product => <Product
+  //                 key={product.id}
+  //                 price={product.price}
+  //                 id={ product.id}
+  //                 name={product.name}
+  //                 image={product.image}
+  //             />)
+  //             setProducts(mappedProducts)
+  //         }
+  //     }).catch(error=> {
+  //         console.log(error)
+  //     })
+  // },[])
 
-  useEffect(()=>{
-      fetchRecentProducts().then(result => {
-          console.log(result)
-          if(result.error === "") {
-              let products = result.data
-              let mappedProducts = products.map(product => <Product
-                  key={product.id}
-                  price={product.price}
-                  id={ product.id}
-                  name={product.name}
-                  image={product.image}
-              />)
-              setProducts(mappedProducts)
-          }
-      }).catch(error=> {
-          console.log(error)
-      })
-  },[])
-
-
-    useEffect(() => {
-        let tempList1:JSX.Element[] = []
-        let tempList2 :JSX.Element[]= []
-        let tempList3:JSX.Element[] = []
-
-        products.forEach((product, index) => {
-            if (index % 3 === 0) {
-                tempList3.push(product)
-            }
-            if (index % 3 === 1) {
-                tempList1.push(product)
-            }
-            if (index % 3 === 2) {
-                tempList2.push(product)
-            }
-
-            setList1(tempList1)
-            setList2(tempList2)
-            setList3(tempList3)
-        })
-    }, [products])
+    //
+    // useEffect(() => {
+    //     let tempList1:JSX.Element[] = []
+    //     let tempList2 :JSX.Element[]= []
+    //     let tempList3:JSX.Element[] = []
+    //
+    //     products.forEach((product, index) => {
+    //         if (index % 3 === 0) {
+    //             tempList3.push(product)
+    //         }
+    //         if (index % 3 === 1) {
+    //             tempList1.push(product)
+    //         }
+    //         if (index % 3 === 2) {
+    //             tempList2.push(product)
+    //         }
+    //
+    //         setList1(tempList1)
+    //         setList2(tempList2)
+    //         setList3(tempList3)
+    //     })
+    // }, [products])
 
 
     const drawerWidth = 240;
