f6c7bb0ec1344ef775f12ef4258b8390c3fca946
Andrei Avram <avramandreitiberiu@gmail.com>
2020-12-10 11:38:48 +0000

    -added dynamic rating

--- src/pages/ExpandedProduct.tsx
+++ src/pages/ExpandedProduct.tsx
@@ -8,7 +8,9 @@ import {ApiError} from "../repositories/CallRunner";
 import StarIcon from '@material-ui/icons/Star';
 import {fetchReviewsForProduct} from "../repositories/ReviewsRepository";
 import {Review} from "../entities/Review";
-
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import {StarHalf} from "@material-ui/icons";
 
 export default function ExpandedProduct(){
 
@@ -16,6 +18,8 @@ export default function ExpandedProduct(){
 
     const [product, setProduct] = useState<ProductModel>()
     const [reviews,setReviews] = useState<Array<Review>>([])
+    const [productRating,setProductRating] = useState(0)
+
 
     useEffect(()=>{
         const productID = window.location.href.substring(window.location.href.lastIndexOf('/') + 1)
@@ -36,6 +40,15 @@ export default function ExpandedProduct(){
         }
     },[])
 
+    useEffect(()=>{
+        let total = 0
+        reviews.forEach(review=>{
+            total += review.rating
+        })
+        setProductRating(total/reviews.length)
+
+    },[reviews])
+
     const useStyles = makeStyles({
         root: {
             minWidth: 275,
@@ -55,132 +68,152 @@ export default function ExpandedProduct(){
 
     const classes = useStyles();
 
-    const ReviewLayout = (review:Review)=>  <Card className={classes.root} key = {review.id}>
-        <CardContent >
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-                12/09/2020
-            </Typography>
-            <Typography variant="h5" component="h2">
-                Andrei Avram
-            </Typography>
-            <div>
-                <StarIcon  htmlColor={"#ffd500"}/>
-                <StarIcon  htmlColor={"#ffd500"}/>
-                <StarIcon   htmlColor={"#ffd500"}/>
-                <StarIcon  htmlColor={"#ffd500"}/>
-                <StarIcon htmlColor={"#ffd500"}/>
-            </div>
-            <Typography variant="body2" component="p" className={"mt-3"}>
-                This is a beautiful girl
-            </Typography>
-        </CardContent>
+    function getStarsBasedOnRating(rating:number){
+        let stars = []
+        for(let i =1; i <= rating;i++) {
+            stars.push(<StarIcon htmlColor={"#ffd500"}/>)
+        }
+        if(rating %1 !==0){
+            stars.push(<StarHalf htmlColor={"#ffd500"}/>)
+        }
+        for(let j=stars.length+1;j<=5;j++){
+            stars.push(<StarBorderIcon htmlColor={"#ffd500"}/>)
+        }
+        return stars
+    }
 
-    </Card>
+    const ReviewLayout = (review:Review)=> {
+
+        return(
+        <Card className={classes.root} key={review.id}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    {review.created}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                    Andrei Avram
+                </Typography>
+                <div>
+                    {getStarsBasedOnRating(review.rating)}
+                </div>
+                <Typography variant="body2" component="p" className={"mt-3"}>
+                    {review.description}
+                </Typography>
+            </CardContent>
+
+        </Card>
+        )
+    }
 
     return (
         <div>
-        <div className={"row mt-5"}>
-            <div className={"col-md"}>
-                <CarouselImages images={product !==undefined ? product.images : []} />
-            </div>
-            <div className={"col-md"}>
-                {  product &&
-                    <Typography
-                        variant="h3">
-                        {product?.name}
-                    </Typography>
-                }
+            {
+                product &&
+
                 <div>
-                <div>
-                        <StarIcon htmlColor={"#ffd500"}/>
-                        <StarIcon htmlColor={"#ffd500"}/>
-                        <StarIcon htmlColor={"#ffd500"}/>
-                        <StarIcon htmlColor={"#ffd500"}/>
-                        <StarIcon htmlColor={"#ffd500"}/>
+                    <div className={"row mt-5"}>
+                        <div className={"col-md"}>
+                            <CarouselImages images={product.images }/>
+                        </div>
+                        <div className={"col-md"}>
+                            {product &&
+                            <Typography
+                                variant="h3">
+                                {product?.name}
+                            </Typography>
+                            }
+                            <div>
+                                <div>
+                                    {
+                                        getStarsBasedOnRating(productRating)
+                                    }
+                                    <span  className={"align-middle ml-2"}>{productRating}</span>
+                                </div>
 
-                </div>
-                    <Typography
-                        color={"primary"}
-                        variant = "subtitle1">
-                        7 reviews
-                    </Typography>
-                </div>
-                {
-                    product &&
-                    <Typography
-                        variant="subtitle1">
-                        {product.description}
-                    </Typography>
+                                <Typography
+                                    color={"primary"}
+                                    variant="subtitle1">
+                                    {reviews.length} reviews
+                                </Typography>
+                            </div>
+                            {
+                                product &&
+                                <Typography
+                                    variant="subtitle1">
+                                    {product.description}
+                                </Typography>
 
-                }
-                    <Typography
-                    variant = "subtitle1">
-                   Products in stock : <span style={{color : "blue"}}>{product?.stock}</span>
-                </Typography>
-                <Typography>
-                </Typography>
+                            }
+                            <Typography
+                                variant="subtitle1">
+                                Products in stock : <span style={{color: "blue"}}>{product?.stock}</span>
+                            </Typography>
+                            <Typography>
+                            </Typography>
 
-        </div>
-            <div className={"col-md"}>
-                <Paper elevation={1} >
-                    <div style={{padding: "15px"}} >
-                        <Typography
-                            variant = "h4">
-                            £{product?.price}
-                        </Typography>
-                        { product &&
-                        <Button variant="contained" color="primary" className={"mt-3"} onClick={() => {
-                            context.addProduct(product.productID)
-                        }}>
-                            Add to basket
-                        </Button>
-                        }
+                        </div>
+                        <div className={"col-md"}>
+                            <Paper elevation={1}>
+                                <div style={{padding: "15px"}}>
+                                    <Typography
+                                        variant="h4">
+                                        £{product?.price}
+                                    </Typography>
+                                    {product &&
+                                    <Button variant="contained" color="primary" className={"mt-3"} onClick={() => {
+                                        context.addProduct(product.productID)
+                                    }}>
+                                        Add to basket
+                                    </Button>
+                                    }
+                                </div>
+                            </Paper>
+
+                        </div>
                     </div>
-                </Paper>
+                    <Typography
+                        variant="h4"
+                        className={"mt-5"}
+                        style={{margin: "auto"}}
+                    >
+                        Reviews
+                    </Typography>
 
-            </div>
-            </div>
-            <Typography
-                variant = "h4"
-                className={"mt-5"}
-                style={{margin:"auto"}}
-            >
-                Reviews
-            </Typography>
+                    <div className={"row mt-5"}>
+                        <div className={"col"}>
+                            {
+                                // eslint-disable-next-line array-callback-return
+                                reviews.map((review, index) => {
+                                    if (index % 3 === 0 || index === 0) {
+                                        return ReviewLayout(review)
+                                    }
+                                })
+                            }
 
-            <div className={"row mt-5"}>
-                <div className={"col"}>
-                    {
-                        // eslint-disable-next-line array-callback-return
-                        reviews.map((review,index) =>{
-                            if(index %3 ===0 || index === 0){
-                                return ReviewLayout(review)
+                        </div>
+                        <div className={"col"}>
+                            {
+                                // eslint-disable-next-line array-callback-return
+                                reviews.map((review, index) => {
+                                    if (index % 3 === 1) {
+                                        return ReviewLayout(review)
+                                    }
+                                })
                             }
-                        })
-                    }
-
-                </div>
-                <div className={"col"}>
-                    {
-                        // eslint-disable-next-line array-callback-return
-                        reviews.map((review,index) =>{
-                            if(index %3 === 1){
-                                return ReviewLayout(review)
+                        </div>
+                        <div className={"col"}>
+                            {
+                                // eslint-disable-next-line array-callback-return
+                                reviews.map((review, index) => {
+                                    if (index % 3 === 2) {
+                                        return ReviewLayout(review)
+                                    }
+                                })
                             }
-                        })
-                    }
+                        </div>
+                    </div>
                 </div>
-                <div className={"col"}>
-                    {
-                        // eslint-disable-next-line array-callback-return
-                        reviews.map((review,index) =>{
-                            if(index %3 === 2){
-                                return ReviewLayout(review)
-                            }
-                        })
-                    }
-                </div>
-            </div>
+            }
         </div>
     )
 }
